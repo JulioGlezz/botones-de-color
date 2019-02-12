0167c502e0e0300c59ada0440e108a8b5cef5898
JulioGlezz <36119020+JulioGlezz@users.noreply.github.com>
2019-02-12 12:57:30 -0500

    Update bot_colors.js

--- js/bot_colors.js
+++ js/bot_colors.js
@@ -1,79 +1,64 @@
 function color_rojo()
 {
-    document.formulario.area_texto.style.background="red";
+    document.formularioB.area_texto.style.background="red";
 }
-
 function color_verde()
 {
-    document.formulario.area_texto.style.background="green";
+    document.formularioB.area_texto.style.background="green";
 }
-
 function color_amarillo()
 {
-    document.formulario.area_texto.style.background="yellow";
+    document.formularioB.area_texto.style.background="yellow";
 }
-
 function color_azul()
 {
-    document.formulario.area_texto.style.background="blue";
+    document.formularioB.area_texto.style.background="blue";
 }
-
 function texto_blanco()
 {
-    document.formulario.area_texto.style.color="white";
+    document.formularioB.area_texto.style.color="white";
 }
-
 function texto_negro()
 {
-    document.formulario.area_texto.style.color="black";
+    document.formularioB.area_texto.style.color="black";
 }
-
 function texto_azul()
 {
-    document.formulario.area_texto.style.color="blue";
+    document.formularioB.area_texto.style.color="blue";
 }
-
 function texto_amarillo()
 {
-    document.formulario.area_texto.style.color="yellow";
+    document.formularioB.area_texto.style.color="yellow";
 }
-
 function texto_12pt()
 {
-    document.formulario.area_texto.style.fontSize="12pt";
+    document.formularioB.area_texto.style.fontSize="12pt";
 }
-
 function texto_18pt()
 {
-    document.formulario.area_texto.style.fontSize="18pt";
+    document.formularioB.area_texto.style.fontSize="18pt";
 }
-
 function texto_24pt()
 {
-    document.formulario.area_texto.style.fontSize="24pt";
+    document.formularioB.area_texto.style.fontSize="24pt";
 }
-
 function texto_30pt()
 {
-    document.formulario.area_texto.style.fontSize="30pt";
+    document.formularioB.area_texto.style.fontSize="30pt";
 }
-
 function texto_arial()
 {
-    document.formulario.area_texto.style.fontFamily="Arial";
+    document.formularioB.area_texto.style.fontFamily="Arial";
 }
-
 function texto_courier()
 {
-    document.formulario.area_texto.style.fontFamily="Courier";
+    document.formularioB.area_texto.style.fontFamily="Courier";
 }
-
 function texto_verdana()
 {
-    document.formulario.area_texto.style.fontFamily="Verdana";
+    document.formularioB.area_texto.style.fontFamily="Verdana";
 }
-
 function texto_kristen()
 {
-    document.formulario.area_texto.style.fontFamily="Kristen ITC";
+    document.formularioB.area_texto.style.fontFamily="Kristen ITC";
 }
